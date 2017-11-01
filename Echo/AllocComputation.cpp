7a91b931535385ce7fa89dcb5425b1078db8575c
Miloš Šimek <tjsnake@me.com>
2017-11-01 14:14:28 +0100

    Adds new file AllocComputation.

---
+++ Echo/AllocComputation.cpp
@@ -0,0 +1,9 @@
+//
+//  AllocComputation.cpp
+//  Echo
+//
+//  Created by Miloš Šimek on 01/11/2017.
+//  Copyright © 2017 Miloš Šimek. All rights reserved.
+//
+
+#include "AllocComputation.hpp"
---
+++ Echo/AllocComputation.hpp
@@ -0,0 +1,16 @@
+//
+//  AllocComputation.hpp
+//  Echo
+//
+//  Created by Miloš Šimek on 01/11/2017.
+//  Copyright © 2017 Miloš Šimek. All rights reserved.
+//
+
+#ifndef AllocComputation_hpp
+#define AllocComputation_hpp
+
+// AllocComputation computes allocation size needed for hash table.
+// It was created because realocations during computation become bottleneck
+// when computing large data on parallel systems.
+
+#endif /* AllocComputation_hpp */
