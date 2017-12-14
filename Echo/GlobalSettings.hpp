078b35fc7ce521e68ece3ae5c05b3585b3fc1fce
Miloš Šimek <tjsnake@me.com>
2017-12-14 10:34:39 +0100

    Bumps version number from alfa to full

--- Echo/GlobalSettings.hpp
+++ Echo/GlobalSettings.hpp
@@ -10,7 +10,7 @@
 #define EchoErrorCorrection_GlobalSettings_h
 
 #define APPLICATION_NAME_STRING     "Echo"
-#define APPLICATION_VERSION_STRING  "2.1.0 (alfa)"
+#define APPLICATION_VERSION_STRING  "2.1.0"
 
 #define RETURN_SUCCESS        0
 #define RETURN_INC_OPT        1
